b3c6a13cb1971e38249a745c1544ecee9d9fa9c9
Sam Selikoff <sam.selikoff@gmail.com>
2022-05-16 19:14:27 -0400

    begin of vid?

--- app/root.jsx
+++ app/root.jsx
@@ -28,7 +28,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-gray-100">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,3 +1,137 @@
+import faker from "@faker-js/faker";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+
+faker.seed(123);
+const people = faker.datatype.array(20).map(() => {
+  let firstName = faker.name.firstName();
+  let lastName = faker.name.lastName();
+  let name = `${firstName} ${lastName}`;
+  let email = faker.internet.email(firstName, lastName).toLowerCase();
+
+  return {
+    name,
+    title: faker.name.jobTitle(),
+    email,
+    role: faker.name.jobType(),
+  };
+});
+
 export default function Index() {
-  return <p className="text-5xl">Hello, world</p>;
+  return (
+    <div className="max-w-6xl pt-16 mx-auto lg:pt-32">
+      <div className="px-4 sm:px-6 lg:px-8">
+        <div className="sm:flex sm:items-center">
+          <div className="sm:flex-auto">
+            <h1 className="text-xl font-semibold text-gray-900">Users</h1>
+            <p className="mt-2 text-sm text-gray-700">
+              A list of all the users in your account including their name,
+              title, email and role.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col mt-8">
+          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-300">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                      >
+                        <a href="#" className="inline-flex group">
+                          Name
+                          <span className="flex-none invisible ml-2 text-gray-400 rounded group-hover:visible group-focus:visible">
+                            <ChevronDownIcon
+                              className="w-5 h-5"
+                              aria-hidden="true"
+                            />
+                          </span>
+                        </a>
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        <a href="#" className="inline-flex group">
+                          Title
+                          <span className="flex-none ml-2 text-gray-900 bg-gray-200 rounded group-hover:bg-gray-300">
+                            <ChevronDownIcon
+                              className="w-5 h-5"
+                              aria-hidden="true"
+                            />
+                          </span>
+                        </a>
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        <a href="#" className="inline-flex group">
+                          Email
+                          <span className="flex-none invisible ml-2 text-gray-400 rounded group-hover:visible group-focus:visible">
+                            <ChevronDownIcon
+                              className="flex-none invisible w-5 h-5 ml-2 text-gray-400 rounded group-hover:visible group-focus:visible"
+                              aria-hidden="true"
+                            />
+                          </span>
+                        </a>
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        <a href="#" className="inline-flex group">
+                          Role
+                          <span className="flex-none invisible ml-2 text-gray-400 rounded group-hover:visible group-focus:visible">
+                            <ChevronDownIcon
+                              className="flex-none invisible w-5 h-5 ml-2 text-gray-400 rounded group-hover:visible group-focus:visible"
+                              aria-hidden="true"
+                            />
+                          </span>
+                        </a>
+                      </th>
+                      <th
+                        scope="col"
+                        className="relative py-3.5 pl-3 pr-4 sm:pr-6"
+                      >
+                        <span className="sr-only">Edit</span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200">
+                    {people.map((person) => (
+                      <tr key={person.email}>
+                        <td className="py-4 pl-4 pr-3 text-sm font-medium text-gray-900 whitespace-nowrap sm:pl-6">
+                          {person.name}
+                        </td>
+                        <td className="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">
+                          {person.title}
+                        </td>
+                        <td className="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">
+                          {person.email}
+                        </td>
+                        <td className="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">
+                          {person.role}
+                        </td>
+                        <td className="relative py-4 pl-3 pr-4 text-sm font-medium text-right whitespace-nowrap sm:pr-6">
+                          <a
+                            href="#"
+                            className="text-indigo-600 hover:text-indigo-900"
+                          >
+                            Edit<span className="sr-only">, {person.name}</span>
+                          </a>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
